3dfced0c9c2b015091a3bdd433b5128de45c3b43
SunPeifeng <g7733pf@gmail.com>
2013-11-03 15:42:05 +0800

    Initial Commit

---
+++ ZQNews/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ZQNews
+//
+//  Created by john fg on 13-11-3.
+//  Copyright (c) 2013年 john fg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ZQNews/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ZQNews
+//
+//  Created by john fg on 13-11-3.
+//  Copyright (c) 2013年 john fg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
